714876653df3546f450c730122043d1e8214f0b0
Rakshit <140839574+Rakshitram07@users.noreply.github.com>
2025-07-31 17:25:50 +0530

    Lamda.tf

--- Lamda.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lambda_function" "my_lambda" {
-  function_name = "my-lambda"
-  filename      = "function.zip"
-  role          = aws_iam_role.lambda_exec.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-}
